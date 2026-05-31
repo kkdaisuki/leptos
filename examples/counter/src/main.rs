456bee160c5a57a939e56d12d88bb1ce64e3f573
kkdaisuki <x@func.moe>
2026-05-31 16:39:19 +0800

    feat: modify counter example to showcase generated props' view method

--- examples/counter/src/main.rs
+++ examples/counter/src/main.rs
@@ -1,10 +1,15 @@
-use counter::SimpleCounter;
+use counter::SimpleCounterProps;
 use leptos::prelude::*;
 
 pub fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
     mount_to_body(|| {
-        view! { <SimpleCounter initial_value=0 step=1/> }
+        // view! { <SimpleCounter initial_value=0 step=1/> }
+        SimpleCounterProps::builder()
+            .initial_value(0)
+            .step(1)
+            .build()
+            .view()
     })
 }
